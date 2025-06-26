e9793d5ebce3940b8b10caa9804ea66ccc1dcdcc
gjparts <gjparts@gmail.com>
2025-06-26 09:54:09 -0600

    commit 2606202524381

--- 26_funciones/main.cpp
+++ 26_funciones/main.cpp
@@ -11,5 +11,18 @@ int main(){
 	cout << sumar(4.5f,2.4f) << endl;
 	cout << restar(3.2,1.1) << endl;
 	
+	//cuando una funcion tiene guard clauses y es propensa a
+	//lanzar una excepcion, esta debe ir en un bloque try-catch
+	try{
+		cout << dividir(5.0,2.0) << endl;	
+		cout << dividir(3.0,0) << endl;
+	}catch(exception &ex){
+		cout << ex.what() << endl;
+	}
+	//observe que para extraer el mensaje de error de una excepcion
+	//debe colocar un ampersand & a la izquierda del objeto ex
+	//y luego al imprimir ejecuta la funcion what() de ex
+	
+	
 	return 777;
 }
